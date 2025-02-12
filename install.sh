2bc889b6a54df2dd5542e7836b4a8d4543013c77
dan0102dan <me@dan0102dan.ru>
2025-02-12 22:52:16 +0300

    настроен автозапуск веб-интерфейса после перезагрузки роутера

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
 
+SERVICE_NAME="kvas-wui"
+KVAS_DIR="/opt/etc/$SERVICE_NAME"
+
 # Функция для вывода сообщения в рамке с очисткой экрана
 print_message() {
-    clear
-    echo "╔══════════════════════════════════════════════╗"
-    printf "║  %s\n" "$1"
-    echo "╚══════════════════════════════════════════════╝"
-    sleep 1
+    PURPLE="\033[1;35m"
+    NC="\033[0m"
+    
+    clean_msg=$(echo -e "$1" | sed -E "s/\\\033\[[0-9;]*m//g")
+    width=$((${#clean_msg} + 6))
+    top=$(printf '╭%.0s─' $(seq 1 $width)) 
+    bottom=$(printf '╰%.0s─' $(seq 1 $width))
+    
+    echo -e "\n${PURPLE}$top╮${NC}"
+    echo -e "${PURPLE}│ ➤  $1${NC}"
+    echo -e "${PURPLE}$bottom╯${NC}"
 }
 
 # Обновление пакетов и установка git-http и curl
 print_message "Обновление пакетов, пожалуйста подождите"
-opkg update > /dev/null 2>&1
-opkg install git-http curl > /dev/null 2>&1
+opkg update
+opkg install git-http curl
 
 # Определение архитектуры системы
 print_message "Определение архитектуры системы"
@@ -35,28 +44,50 @@ esac
 echo "Определена архитектура: $ARCHITECTURE"
 sleep 1
 
-# Загрузка бинарного файла Go-сервера для нужной архитектуры
+# Создание целевой директории и перенос файлов
+print_message "Создание $KVAS_DIR"
+rm -rf "$KVAS_DIR"; mkdir "$KVAS_DIR"
+
+# Загрузка бинарного файла Go-сервера
+BINARY_PATH="$KVAS_DIR/$FILE_NAME"
 print_message "Загрузка бинарного файла Go-сервера для $ARCHITECTURE"
-rm -rf "./$FILE_NAME" > /dev/null 2>&1
-curl -L -s "https://github.com/dan0102dan/kvas-wui/releases/latest/download/$FILE_NAME" -o "./$FILE_NAME"
-chmod +x "./$FILE_NAME"
-echo "Downloaded $FILE_NAME for architecture $ARCHITECTURE"
+curl -L "https://github.com/dan0102dan/kvas-wui/releases/latest/download/$FILE_NAME" -o "$BINARY_PATH"
+chmod +x "$BINARY_PATH"
 sleep 1
 
-# Завершение процессов, занимающих порты 5000 или 3000
+# Завершение процессов на портах
 print_message "Завершение процессов на портах 5000/3000"
 kill -9 $(netstat -tulpn | grep -E ':5000|:3000' | awk '{print $7}' | cut -d'/' -f1) 2>/dev/null || true
-echo "Процессы на портах 5000/3000 завершены"
 sleep 1
 
-# Клонирование содержимого ветки gh-pages в папку build
-print_message "Клонирование содержимого ветки gh-pages в папку 'build'"
-rm -rf ./build > /dev/null 2>&1
-git clone --depth 1 --branch gh-pages https://github.com/dan0102dan/kvas-wui.git ./build > /dev/null 2>&1
-echo "GitHub Pages content успешно склонирован в ./build"
+# Клонирование и обработка build
+git clone --depth 1 --branch gh-pages https://github.com/dan0102dan/kvas-wui.git "$KVAS_DIR/build"
+find "$KVAS_DIR/build" -type f ! -path "/static/*" -exec sed -i'' -e 's|/kvas-wui|./|g' {} +
+
+# Настройка автозапуска
+print_message "Настройка автозапуска программы"
+
+# Конфигурация сервиса
+INIT_SCRIPT="/opt/etc/init.d/S99$SERVICE_NAME"
+
+cat << EOF > "$INIT_SCRIPT"
+#!/bin/sh
+
+ENABLED=yes
+PROCS="$BINARY_PATH"
+ARGS=""
+PREARGS=""
+DESC="Kvas Web UI Service"
+
+. /opt/etc/init.d/rc.func
+EOF
+
+chmod +x "$INIT_SCRIPT"
 sleep 1
 
-# Замена всех путей /kvas-wui на ./
-find ./build -type f -exec sed -i 's|/kvas-wui|./|g' {} \;
-echo "Пути заменены в каталоге ./build"
+# Запуск сервиса
+print_message "Запуск $SERVICE_NAME..."
+"$INIT_SCRIPT" start
+echo "Проверка статуса: $INIT_SCRIPT status"
+echo "Логи: tail -f /var/log/messages | grep $SERVICE_NAME"
 sleep 1
